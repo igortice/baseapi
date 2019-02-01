3222e1f4c5195692bc755974ac1d16457c0d4388
Igor Rocha <igortice@gmail.com>
2019-01-31 22:44:35 -0300

    better

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,8 @@
 # frozen_string_literal: true
 
 Rails.application.routes.draw do
-  scope module: 'api' do
-    namespace :v1 do
+  namespace :'api' do
+    namespace :'v1' do
       mount_devise_token_auth_for 'User', at: 'auth'
 
       resources :users
